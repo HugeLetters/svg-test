b47abd621b64d3c599d87c4dd672d5e0313b1a4b
hugeletters <ckuka71@gmail.com>
2024-04-23 19:09:35 +0200

    layout with csr disabled

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,24 @@
+<script lang="ts">
+	import { count } from './count';
+</script>
+
 <nav>
-	<a href="/inline">inline</a>
-	<a href="/sprite">sprite</a>
+	<a href="/inline?count={$count}">inline</a>
+	<a href="/sprite?count={$count}">sprite</a>
 </nav>
-<slot></slot>
+<form action="">
+	<label>
+		chunk count:
+		<input min="1" max="10000" name="count" type="number" value={$count} />
+	</label>
+</form>
+<div class="root">
+	<slot></slot>
+</div>
+
+<style>
+	.root {
+		fill: black;
+		stroke: black;
+	}
+</style>
