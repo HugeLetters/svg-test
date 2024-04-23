9a985c2447d13a2dfd833adc445f0b050d17dc55
hugeletters <ckuka71@gmail.com>
2024-04-23 17:51:54 +0200

    added icons

---
+++ src/lib/icon/arrow.svelte
@@ -0,0 +1,10 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+	<path d="M20,12H11C2,12,2,5,2,5" fill="none" stroke-linecap="round" stroke-width="3"></path>
+	<polyline
+		points="15 5 22 12 15 19"
+		fill="none"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		stroke-width="3"
+	></polyline>
+</svg>
