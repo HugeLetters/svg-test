a577d262d0956213ba2e6c462303b0ae2c29cb6d
hugeletters <ckuka71@gmail.com>
2024-04-23 19:10:04 +0200

    add inline group

---
+++ src/lib/icon/inline/group.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Arrow from '../asset/arrow.svelte';
+	import Bell from '../asset/bell.svelte';
+	import Envelope from '../asset/envelope.svelte';
+	import Glass from '../asset/glass.svelte';
+	import Heart from '../asset/heart.svelte';
+</script>
+
+<Arrow />
+<Bell />
+<Envelope />
+<Glass />
+<Heart />
