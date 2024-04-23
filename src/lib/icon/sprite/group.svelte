87968945d83f07b574cb51d8ba72b5e12883376d
hugeletters <ckuka71@gmail.com>
2024-04-23 19:10:08 +0200

    add sprite group

---
+++ src/lib/icon/sprite/group.svelte
@@ -0,0 +1,26 @@
+<svg>
+	<use href="#arrow" />
+</svg>
+
+<svg>
+	<use href="#bell" />
+</svg>
+
+<svg>
+	<use href="#envelope" />
+</svg>
+
+<svg>
+	<use href="#glass" />
+</svg>
+
+<svg>
+	<use href="#heart" />
+</svg>
+
+<style>
+	svg {
+		width: 24px;
+		height: 24px;
+	}
+</style>
---
+++ src/lib/icon/sprite/sprite.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Arrow from '../asset/arrow.svelte';
+	import Bell from '../asset/bell.svelte';
+	import Envelope from '../asset/envelope.svelte';
+	import Glass from '../asset/glass.svelte';
+	import Heart from '../asset/heart.svelte';
+</script>
+
+<svg class="root">
+	<symbol id="arrow">
+		<Arrow />
+	</symbol>
+	<symbol id="bell">
+		<Bell />
+	</symbol>
+	<symbol id="envelope">
+		<Envelope />
+	</symbol>
+	<symbol id="glass">
+		<Glass />
+	</symbol>
+	<symbol id="heart">
+		<Heart />
+	</symbol>
+</svg>
+
+<style>
+	.root {
+		display: none;
+	}
+</style>
