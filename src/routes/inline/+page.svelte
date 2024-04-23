5c56c78050652e004270a31a752bf5b264b84bca
hugeletters <ckuka71@gmail.com>
2024-04-23 19:10:13 +0200

    pages

--- src/routes/inline/+page.svelte
+++ src/routes/inline/+page.svelte
@@ -1,1 +1,8 @@
-inline
+<script lang="ts">
+	import InlineGroup from '$lib/icon/inline/group.svelte';
+	import { count } from '../count';
+</script>
+
+{#each { length: $count } as _}
+	<InlineGroup />
+{/each}
--- src/routes/sprite/+page.svelte
+++ src/routes/sprite/+page.svelte
@@ -1,1 +1,10 @@
-sprite
+<script>
+	import SpriteGroup from '$lib/icon/sprite/group.svelte';
+	import Sprite from '$lib/icon/sprite/sprite.svelte';
+	import { count } from '../count';
+</script>
+
+<Sprite />
+{#each { length: $count } as _}
+	<SpriteGroup />
+{/each}
